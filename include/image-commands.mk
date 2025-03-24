fec8a5f9095cdc10e955787ffad68db9fa1dc2f9
Dem Demiurge <51953496+Demiurge67@users.noreply.github.com>
2025-03-24 16:26:52 +0300

    Add support AX80 direct image -> Update image-commands.mk

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -565,6 +565,26 @@ define Build/sysupgrade-tar
 		$@
 endef
 
+define Build/tplink-mkimage-ubi
+ 	$(TOPDIR)/scripts/tplink-mkimage-ubi.py \
+ 		--create \
+ 		--device-name "$(TPLINK_DEVICE)" \
+ 		--device-ver "$(TPLINK_VERSION)" \
+ 		--special-ids "$(TPLINK_IDS)" \
+ 		--version "$(TPLINK_FWVER)" \
+ 		--rootfs "$@" \
+ 		"$(@).new"
+ 	@mv "$(@).new" "$@"
+ endef
+ 
+define Build/tplink-image-2022
+ 	$(TOPDIR)/scripts/tplink-mkimage-2022.py  \
+ 		--create $@.new \
+ 		--rootfs $@ \
+ 		--support "$(TPLINK_SUPPORT_STRING)"
+ 	@mv $@.new $@
+ endef
+ 
 define Build/tplink-safeloader
 	-$(STAGING_DIR_HOST)/bin/tplink-safeloader \
 		-B $(TPLINK_BOARD_ID) \
